9b57555db716c5fda3376ae888ebfc000f796df2
kkeducation12345 <k.k.e.ducationblr@gmail.com>
2024-08-30 12:44:33 +0530

    Create DemoApp.java

---
+++ src/main/java/com/kk/springmongo/DemoApp.java
@@ -0,0 +1,22 @@
+package com.kk.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.kk.*")
+public class DemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
